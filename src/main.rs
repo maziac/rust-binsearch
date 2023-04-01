a05edf7e2d8b316ff98c6ed36653896b8e2a1509
maziac <thomas.busse@gmx.net>
2023-04-01 18:38:49 +0200

    Reading a binary buffer.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::{env};
-
+use std::io::{self, Write};
+use std::io::Read;
+use std::io::BufReader;
+use std::fs::File;
 
 
 
 fn main() {
+    let mut buffer: Vec<u8> = Vec::new();
+    let mut file_name: String;
     let mut offs = 0;
-    let mut size = 0;
+    let mut size: i32;
 
     let args: Vec<String> = env::args().collect();
     let mut args_iter = args.iter();
@@ -27,19 +32,19 @@ fn main() {
              args_help();
         }
         else if arg == "--offs" {
-            args_offs();
+            let o = get_next(args_iter.next(), "Expected a size.");
+            println!("offs: {}", o);
+            offs = o.parse::<i32>().unwrap();
         }
-        else if arg == "--reloffs" {
+        else if arg == "--roffs" {
             args_reloffs();
         }
         else if arg == "--size" {
-            let s = get_next(args_iter, "Expected a size.");
+            let s = get_next(args_iter.next(), "Expected a size.");
             println!("size: {}", s);
-
             size = s.parse::<i32>().unwrap();
-            //dump(offs, size);
+            dump(&buffer, offs, size);
             offs += size;
-            args_size();
         }
         else if arg == "--search" {
             args_search();
@@ -48,10 +53,45 @@ fn main() {
             args_format();
         }
         else {
-            abort(&["Unknown argument: ", arg].concat());
+            // It is the filename. Open file.
+            append_file(&mut buffer, arg);
+
+
+            //abort(&["Unknown argument: ", arg].concat());
         }
     }
 }
+
+
+
+fn append_file(buffer: &mut Vec<u8>, spath: &str) {
+    // let path = Path::new(spath);
+    // let file = match File::open(&path) {
+    // Err(why)=>panic("couldn't open {}: {}", path.display(), why),
+    //     Ok(file) => file,
+    // };
+
+    // Open file
+    let file = match File::open(&spath) {
+        Err(why) => panic!("Couldn't open {}: {}", spath, why),
+        Ok(file) => file,
+    };
+
+    // Read and append bytes
+    let mut reader = BufReader::new(file);
+
+    // Read file into vector.
+    match reader.read_to_end(buffer) {
+        Err(why) => panic!("Couldn't read {}: {}", spath, why),
+        Ok(_size) => (),
+    };
+}
+
+
+fn dump(buffer: & Vec<u8>, offset:i32, size:i32) {
+    io::stdout().write_all(buffer).unwrap();
+}
+
 
 
 fn abort(error_msg: &str) {
@@ -60,12 +100,11 @@ fn abort(error_msg: &str) {
 }
 
 
-fn get_next(iter: impl Iterator<Item=&'a String>, error_msg: &str) { // -> &str {
-    let item = iter.next();
-    if item == None {
+fn get_next<'a>(arg_option: Option<&'a String>, error_msg: &str) -> &'a String {
+    if arg_option == None {
         abort(error_msg);
     }
- //   item.unwrap()
+    arg_option.unwrap()
 }
 
 
@@ -75,7 +114,7 @@ fn args_help() {
 
     println!("--help: Prints this help.");
     println!("--offs offset: Offset from start of file. Moves last position.");
-    println!("--reloffs offset: Offset from last position. Moves last position.");
+    println!("--roffs offset: Offset from last position. Moves last position.");
     println!("--size size: The number of bytes to evaluate. Moves last position.");
     println!("--search token [token ...]: Searches for the first occurrence of tokens. Token can be a decimal of hex number or a string. The search starts at last position.");
     println!("--format format: The output format:");
@@ -94,9 +133,6 @@ fn args_offs() {
 }
 
 fn args_reloffs() {
-}
-
-fn args_size() {
 }
 
 fn args_search() {
